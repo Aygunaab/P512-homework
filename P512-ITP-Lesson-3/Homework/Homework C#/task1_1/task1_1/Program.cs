1e30f151793ab817ec4ffa84fdfce5f6078eab16
Aygunaab <aygunaab@code.edu.az>
2021-08-25 09:49:49 -0400

    task1_1

---
+++ P512-ITP-Lesson-3/Homework/Homework C#/task1_1/task1_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Number;
+            int x = 70000;
+            int y = 8;
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine("Evveline 7 sonuna 8 elave etmek istediyiniz 4 reqemli ededi daxil edin");
+                Number = Convert.ToInt32(Console.ReadLine());
+                if (Number >= 1000 && Number <= 9999)
+                {
+                    int result = (Number + x) * 10 + y;
+                    Console.WriteLine(result);
+                }
+                else
+                {
+                    Console.WriteLine("Daxil etdiyiniz eded 4 reqemli deyil");
+                }
+            }
+           
+
+        }
+    }
+}
